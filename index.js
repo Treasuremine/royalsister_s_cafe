630cdeea381b42acbdc45ef41ccc95889bb5b887
Mallika <132191889+Treasuremine@users.noreply.github.com>
2023-06-08 22:03:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello, World!");
+//index.js
+const http=require('http');
+const server = http.createServer((req, res)=>{
+  res.writehead( 200,{'content-type': 'text/plain'});
+  res.end('hello World \n');
+});
+
+server.listen( 3000, () =>{
+  console.log('Server running at http://localhost:3000/');
+});
+console.log("Hello, royalsister_s_cafe!");
